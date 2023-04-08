5cd59cd9fb370d67b3bc77d95b440e4d5ae98b66
kzw3933 <3099097649@qq.com>
2023-04-08 20:05:45 +0800

    开始读取可执行文件

--- src/rvemu.c
+++ src/rvemu.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "rvemu.h"
 
 int main(int argc, char* argv[]) {
-    printf("Hello rvemu!\n");
+    assert(argc > 1);
+
+    machine_t machine = {0};
+    machine_load_program(&machine, argv[1]);
+
+    printf("entry: %lx\n", machine.mmu_entry);
     return 0;
 }
